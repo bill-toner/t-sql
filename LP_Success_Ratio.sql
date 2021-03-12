c26d80ee299f97f94bc358b25ef510d57263fb85
Bill Toner <toner.bill@gmail.com>
2021-03-12 15:36:40 -0500

    Update LP_Success_Ratio.sql

--- LP_Success_Ratio.sql
+++ LP_Success_Ratio.sql
@@ -1,11 +1,11 @@
 With LP5000 as (Select Convert(Date, requestdate) as [Date], Count(*) as 'LP in 5000'
-			From DB.dbo.loan_comrequest (nolock)
+			From DB.dbo.tb (nolock)
 			Where serverid = 340 
 				and statusid = 5000
 				and Convert(Date, requestdate) >= Convert(Date, GetDate() - 30)
 			Group By Convert(Date, requestdate)),
 	LP6000 as (Select Convert(Date, requestdate) as [Date], Count(*) as 'LP in 6000'
-			From DB.dbo.loan_comrequest (nolock)
+			From DB.dbo.tb (nolock)
 			Where serverid = 340 
 				and statusid = 6000
 				and Convert(Date, requestdate) >= Convert(Date, GetDate() - 30)
